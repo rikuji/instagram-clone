e39cab5f9192ea2bfa237fa10fc6d18f73601283
Lucas <lucasbezerra1701@gmail.com>
2020-04-22 05:39:02 -0300

    backend

---
+++ backend/src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+mongoose.connect(
+  'mongodb+srv://lucas:a10129103@cluster0-0mydt.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  '/files',
+  express.static(path.resolve(__dirname, '..', 'uploads', 'resized'))
+);
+
+app.use(require('./routes'));
+
+server.listen(3333);
